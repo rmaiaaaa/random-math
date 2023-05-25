b63e4af3a0e0690329ab95531d1380cd7e9a8dc7
Rafael Maia de Souza <rmaiadesouza@gmail.com>
2023-05-24 21:02:26 -0300

    Código inicial
    Inclusão do código inicial para a aplicação em estudo.

---
+++ src/RandomMath/RandomMathOperations.py
@@ -0,0 +1,14 @@
+from mimetypes import init
+import math
+import random
+
+class RandomMath:
+    def __init__(self) -> None:
+        pass
+
+    def random_sum(self, number:int = 0) -> int:
+        '''
+        Realiza a soma de um número de entrada com um número aleatório
+        '''
+        random_int = random.randint(1, number)
+        return number + random_int
---
+++ test/test_RandomMathOperations.py
@@ -0,0 +1,18 @@
+import re
+from RandomMath.RandomMathOperations import RandomMath
+
+class TestJustSomeMath:
+
+    def test1(self):
+        
+        #Arrange
+        randomMath = RandomMath()
+        number = 1
+
+        #Act
+        result = randomMath.random_sum(number)
+        
+        #Assert
+        assert result > number
+
+        pass
