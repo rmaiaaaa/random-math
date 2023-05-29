1b62ac34cc42facc08325070bc5c461f3b1fa179
Rafael Maia de Souza <rmaiadesouza@gmail.com>
2023-05-28 22:26:38 -0300

    Segregação de paths para Package CI

---
+++ src/apiRandomMath/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from RandomMath.RandomMathOperations import RandomMath
+
+app = FastAPI()
+math = RandomMath()
+
+@app.get("/")
+async def root():
+    return {"message": "Vamos fazer cálculos aleatórios"}
+
+@app.get("/math/sum/{number}")
+async def random_sum(number: int = 0):
+    return{"message": math.random_sum(number)}
