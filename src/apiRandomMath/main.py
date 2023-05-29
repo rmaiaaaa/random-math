b8498ad202acc5d165e485c1839f8a6c2abf05a1
Rafael Maia de Souza <rmaiadesouza@gmail.com>
2023-05-28 23:22:21 -0300

    Correção code check API CI

--- src/apiRandomMath/main.py
+++ src/apiRandomMath/main.py
@@ -4,11 +4,12 @@ from RandomMath.RandomMathOperations import RandomMath
 app = FastAPI()
 math = RandomMath()
 
+
 @app.get("/")
 async def root():
     return {"message": "Vamos fazer cálculos aleatórios"}
 
+
 @app.get("/math/sum/{number}")
 async def random_sum(number: int = 0):
-    return{"message": math.random_sum(number)}
-  
+    return {"message": math.random_sum(number)}
