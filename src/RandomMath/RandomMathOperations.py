12a6e323a5b1217d15fa55d517d6a800d976ce89
Rafael Maia de Souza <rmaiadesouza@gmail.com>
2023-05-30 18:21:09 -0300

    Correções linting

--- src/RandomMath/RandomMathOperations.py
+++ src/RandomMath/RandomMathOperations.py
@@ -12,11 +12,10 @@ class RandomMath:
         '''
         random_int = random.randint(1, number)
         return number + random_int
-    
-    def random_sub(self, number: int = 0) ->int:
+
+    def random_sub(self, number: int = 0) -> int:
         '''
         Realiza a subtração de um número de entrada por um número aleatório
         '''
         random_int = random.randint(1, number)
         return number - random_int
-
